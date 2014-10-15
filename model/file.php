e7e188dcd00a7f40cff1db88022c540e03e93c5b
Kristian Nordman <tiny@zeus.scripter.se>
2014-10-15 15:19:27 +0200

    asd

--- model/file.php
+++ model/file.php
@@ -11,9 +11,16 @@ class model_file {
     function add_file($filename, $name, $type, $data, $user){
         $filemd5 = md5($data);
         file_put_contents(UPLOAD.$filemd5, $data);
-        $sql = 'insert into file (filename, name, path, type, author, timestamp) values ("'.$filename.'", "'.$name.'", "'.$filemd5.'", "'.$type.'", "'.$user.'", "'.time().'")';
-        model::factory('database')->query($sql);
-        return model::factory('database')->get_connection()->insert_id;
+        $sql = 'insert into file (filename, name, path, type, author, timestamp) values (:filename, :name, :md5, :type, :user, :time)';
+        $params = array(
+                        'filename' => $filename,
+                        'name' => $name,
+                        'md5' => $filemd5,
+                        'type' => $type,
+                        'user' => $user,
+                        'time' => time()
+                    );
+        return model::factory('database')->insert($sql, $params);
     }
     function get_info($id){
         $res = model::factory('database')->query('select * from file where idfile ="'.$id.'"');
